e3a909f692186e14219a3da4e0a3c862134d7e54
dev-rogerinho <luizrssjunior@gmail.com>
2026-05-27 01:19:30 -0300

    Atualizando classe Filme

--- src/Filme.java
+++ src/Filme.java
@@ -1,12 +1,68 @@
 public class Filme {
     private String nome;
     private double valor;
-   
-    public Filme(String nome, double valor) {
+    private int duracao;
+    private String sinopse;
+    private double nota;
+    private int quantidadeCriticos;
+    private Critica[] criticas = new Critica[100];
+    private int qtdCriticas = 0;
+    private boolean emCartaz;
+
+    public Filme(String nome, double valor, int duracao, String sinopse, boolean emCartaz) {
         this.nome = nome;
         this.valor = valor;
+        this.duracao = duracao;
+        this.sinopse = sinopse;
+        this.nota = 0;
+        this.quantidadeCriticos = 0;
+        this.emCartaz = emCartaz;
     }
 
-    public String getNome() { return nome; }
-    public double getValor() { return valor; }
+    public String getNome() {
+        return nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public int getQuantidadeCriticos() {
+        return quantidadeCriticos;
+    }
+
+    public boolean isEmCartaz() {
+    return emCartaz;
+}
+
+    // adicionar nota e recalcula a média
+    public void adicionarNota(double novaNota) {
+        nota = (nota * quantidadeCriticos + novaNota) / (quantidadeCriticos + 1);
+        quantidadeCriticos++;
+    }
+
+    // adicionar crítica no vetor
+    public void adicionarCritica(Critica critica) {
+        criticas[qtdCriticas] = critica;
+        qtdCriticas++;
+    }
+
+    // mostrar críticas
+    public void mostrarCriticas() {
+        for (int i = 0; i < qtdCriticas; i++) {
+            System.out.println(criticas[i].getNome() + " (" + criticas[i].getOrigem() + "): " + criticas[i].getMensagem());;
+    }
+    }
 }
--- src/Sala.java
+++ src/Sala.java
@@ -1,15 +1,29 @@
-public class Sala {
-    private Sessao[] sessoes;
+public class Sala { // alterar
+    private Sessao[] sessoes; // array vetor que guarda varios objetos da sessao
 
     public Sala(int tamanho) {
-        sessoes = new Sessao[tamanho];
+        sessoes = new Sessao[tamanho]; // Instancia o array
     }
 
     public void adicionarSessao(int index, Sessao sessao) {
-        sessoes[index] = sessao;
+        sessoes[index] = sessao; 
     }
 
     public Sessao getSessao(int index) {
         return sessoes[index];
+    }
+
+    public void mostrarSessoes() { // Mostrar sessões
+        System.out.println("=== SESSÕES DISPONÍVEIS ===");
+
+        for (int i = 0; i < sessoes.length; i++) {
+            if (sessoes[i] != null) {
+                System.out.println("Sala: " + sessoes[i].getNumeroSala());
+                System.out.println("Filme: " + sessoes[i].getFilme().getNome());
+                System.out.println("Tipo: " + sessoes[i].getTipoSala());
+                System.out.println("Horário: " + sessoes[i].getHorario());
+                System.out.println("----------------------");
+            }
+        }
     }
 }
